fdb0aa1b9b4cedaf22d69ee785c12eb20a478594
Amiel Cohen <amielc1@gmail.com>
2024-03-05 12:04:25 +0200

    Use standart logs

--- Store.Api/Controllers/QuickOrderController.cs
+++ Store.Api/Controllers/QuickOrderController.cs
@@ -20,7 +20,7 @@ namespace Store.Api.Controllers
         [HttpPost]
         public Guid SubmitQuickOrder(QuickOrder orderInfo)
         {
-            //_logger.LogInformation($"Submitting order for {orderInfo.Quantity} of {orderInfo.ProductId}.");
+            _logger.LogInformation($"Submitting order for {orderInfo.Quantity} of {orderInfo.ProductId}.");
             return _orderLogic.PlaceQuickOrder(orderInfo, 1234); // would get customer id from authN system/User claims
         }
     }
